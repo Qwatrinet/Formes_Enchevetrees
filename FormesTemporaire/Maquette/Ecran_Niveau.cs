74931eced05f0e6dc5c49a0a7d3226a6d9d46515
Grogro21 <74548874+Grogro21@users.noreply.github.com>
2024-11-05 09:47:01 +0100

    temp

---
+++ FormesTemporaire/Maquette/Ecran_Niveau.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maquette
+{
+    public partial class Ecran_Niveau : Form
+    {
+        public Ecran_Niveau()
+        {
+            InitializeComponent();
+        }
+    }
+}
